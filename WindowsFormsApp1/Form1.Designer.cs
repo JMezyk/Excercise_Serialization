add945b3af7dd26a117920d38a153e6e65274051
JMezyk <jordan.mezyk@wp.pl>
2025-01-24 09:29:27 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.item1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // item1ToolStripMenuItem
+            // 
+            this.item1ToolStripMenuItem.Name = "item1ToolStripMenuItem";
+            this.item1ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // item2ToolStripMenuItem
+            // 
+            this.item2ToolStripMenuItem.Name = "item2ToolStripMenuItem";
+            this.item2ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // item3ToolStripMenuItem
+            // 
+            this.item3ToolStripMenuItem.Name = "item3ToolStripMenuItem";
+            this.item3ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 268);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(522, 162);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "SERIALIZE to XML";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 436);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 40);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "DESERIALIZE from XML";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(269, 222);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 40);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "SERIALIZE to JSON";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(269, 436);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 40);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "DESERIALIZE from JSON";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Serialize and deserialize some dummy data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 48);
+            this.label2.MaximumSize = new System.Drawing.Size(200, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 156);
+            this.label2.TabIndex = 12;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 48);
+            this.label3.MaximumSize = new System.Drawing.Size(300, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 104);
+            this.label3.TabIndex = 12;
+            this.label3.Text = " public JSONdata() \r\n { \r\n     privateData1 = 12;\r\n     privateData2 = 988;\r\n    " +
+    " publicInt1 = 888;\r\n     publicInt2 = -1;\r\n     dataTable = new int[] { 4, 6, 56" +
+    "45, 111};\r\n }";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 510);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Serialization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem item1ToolStripMenuItem;
+        private ToolStripMenuItem item2ToolStripMenuItem;
+        private ToolStripMenuItem item3ToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox2;
+        private SaveFileDialog saveFileDialog1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.Json;
+using System.Collections.Specialized;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        #region FieldDefinitions
+            public XMLdata XMLobject;
+            public JSONdata JSONobject;
+        #endregion
+      
+        #region Constructors
+        public Form1()
+        {
+            InitializeComponent();
+            this.XMLobject = new XMLdata();
+            this.JSONobject = new JSONdata();
+        }
+
+        ~Form1()
+        {
+            
+        }
+        #endregion
+        
+    
+         #region Serialization
+        private void button4_Click(object sender, EventArgs e) // SERIALIZE
+        {
+            StreamWriter myStream;
+            XmlSerializer mySerializer;
+            
+            saveFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                if (saveFileDialog1.FileName != null)
+                {
+                    mySerializer = new XmlSerializer(XMLobject.GetType());
+                    myStream = new StreamWriter(saveFileDialog1.FileName, false);
+                    mySerializer.Serialize(myStream,XMLobject);
+                    myStream.Close();
+                
+                  
+                }
+                
+                
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)      // DESERIALIZE
+        {
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+            XMLdata inputData;
+            XmlSerializer mySerializer;
+            FileStream myStream;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FileName != null)
+                {
+                    inputData = new XMLdata();
+                    mySerializer = new XmlSerializer(XMLobject.GetType());
+                    myStream = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                    inputData = (XMLdata)mySerializer.Deserialize(myStream);
+
+                    textBox2.Text = inputData.ToString();
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)      // SERIALIZE JSON
+        {
+            StreamWriter myStream;
+            JsonSerializerOptions options;
+
+            saveFileDialog1.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName != null)
+                {
+                    myStream = new StreamWriter(saveFileDialog1.FileName, false);
+                    options = new JsonSerializerOptions();
+                    options.WriteIndented = true;
+                    myStream.Write(JsonSerializer.Serialize(JSONobject, options));
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)          // DESERIALIZE JSON
+        {
+            openFileDialog1.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+            JSONdata inputData;
+            string JSONbuffer;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FileName != null)
+                {
+                    inputData = new JSONdata();
+                    JSONbuffer = File.ReadAllText(openFileDialog1.FileName);
+                    inputData = JsonSerializer.Deserialize<JSONdata>(JSONbuffer);
+                    textBox2.Text = inputData.ToString();
+                }
+            }
+        }
+        #endregion
+    }
+}
